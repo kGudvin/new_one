3b0efc2aea0415b587278ab79030cf17723a4c9b
Kirill Gudeta <kirillgudeta@gmail.com>
2022-07-17 20:06:34 +0300

    part of navbar

--- src/contexts/ContextProvider.js
+++ src/contexts/ContextProvider.js
@@ -10,10 +10,17 @@ const initialState = {
 }
 export const ContextProvider = ({children}) => {
   const [activeMenu, setActiveMenu] = useState(true)
+  const [isClicked, setIsClicked] = useState(initialState)
+
+  const handleClick = (clicked) => setIsClicked({ ...initialState, [clicked]: true });
+
   return (
     <StateContext.Provider 
     value={{activeMenu,
-      setActiveMenu
+      setActiveMenu,
+      isClicked, 
+      setIsClicked,
+      handleClick
     }}>
       {children}
       </StateContext.Provider> 
